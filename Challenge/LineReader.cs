6be08ba321f59fedc064fcd36bcd0159e35753c1
Anagnostopoulos <kostas_anagnostopoulos@mcafee.com>
2017-04-23 05:43:49 +0100

    Added line reader

---
+++ Challenge/LineReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge
+{
+    class LineReader : ILineReader
+    {
+        public IEnumerable<string> Read(string file)
+        {
+            return System.IO.File.ReadAllLines(file);
+        }
+    }
+}
