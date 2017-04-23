43ceee6a5914a7f17b71b6e0ce7bed60292e6a0f
Anagnostopoulos <kostas_anagnostopoulos@mcafee.com>
2017-04-23 06:01:01 +0100

    Added line parser

---
+++ Challenge/LineParser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge
+{
+    public class LineParser : ILineParser
+    {
+        private const int NUMBER_OF_OCCURENCES_COMMA_CHAR = 1;
+        private const int FIRST_NAME = 0;
+        private const int SECOND_NAME = 1;
+
+        public Tuple<string, string> Parse(string line)
+        {
+            VerifyLineHasContent(line);
+            VerifyLineContainsTheCorrectOccurencesOfCommaChar(line);
+            return new Tuple<string, string>(line.Split(',')[FIRST_NAME], line.Split(',')[SECOND_NAME]);
+        }
+
+        private static void VerifyLineHasContent(string line)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                throw new ArgumentException(nameof(line));
+            }
+        }
+
+        private void VerifyLineContainsTheCorrectOccurencesOfCommaChar(string line)
+        {
+            int numOccurences = OccurencesOfCommaChar(line);
+            if (numOccurences < NUMBER_OF_OCCURENCES_COMMA_CHAR)
+            {
+                throw new ArgumentException($"Missing comma character in line: {line}");
+            }
+            if (numOccurences > NUMBER_OF_OCCURENCES_COMMA_CHAR)
+            {
+                throw new ArgumentException($"More comma characters in line: {line}");
+            }
+        }
+
+        private int OccurencesOfCommaChar(string line)
+        {
+            return line.ToCharArray().Count(s => s == ',');
+        }
+    }
+}
---
+++ ChallengeTests/LineParserTests.cs
@@ -0,0 +1,52 @@
+﻿using Challenge;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChallengeTests
+{
+    [TestClass]
+    public class LineParserTest
+    {
+        private const string LINE_WITHOUT_COMMA = "MPLA_MPLAGKOUX_GKOUX";
+        private const string LINE_WITH_MORE_THAN_ONE_COMMA = "MPLA_MPLA,GKOUX,_GKOUX";
+        private const string FIRST_NAME = "MPLA_MPLA";
+        private const string SECOND_NAME = "GKOUX_GKOUX";
+        private const string CORRECT_LINE = FIRST_NAME + "," + SECOND_NAME;
+
+        private readonly LineParser _parser = new LineParser();
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LineParserShouldNotAcceptNullAsInput()
+        {
+            _parser.Parse(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LineParserShouldNotAcceptEmptyLinesAsInput()
+        {
+            _parser.Parse(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LineMustContainOneComma()
+        {
+            _parser.Parse(LINE_WITHOUT_COMMA);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LineShouldNOTContainMoreThanOneComma()
+        {
+            _parser.Parse(LINE_WITH_MORE_THAN_ONE_COMMA);
+        }
+
+        [TestMethod]
+        public void ShouldSplitTheLineIntoTwoNames()
+        {
+            Assert.AreEqual(new Tuple<string, string>(FIRST_NAME, SECOND_NAME), _parser.Parse(CORRECT_LINE));
+        }
+    }
+}
